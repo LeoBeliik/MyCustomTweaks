5337bfb474b95e245f52f2479bb0718d1f2f02ea
Leo <Leobrac23@gmail.com>
2023-03-25 09:12:26 -0300

    refmap???

--- src/main/java/com/leobeliik/mycustomtweaks/mixins/ManaRepairMixin.java
+++ src/main/java/com/leobeliik/mycustomtweaks/mixins/ManaRepairMixin.java
@@ -19,7 +19,7 @@ public class ManaRepairMixin {
     @Unique
     private List<Item> noRepairItems = new ArrayList<>();
     @Redirect(method = "Lvazkii/botania/common/impl/mana/ManaItemHandlerImpl;requestManaExact(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/entity/player/Player;IZ)Z",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;isEmpty()Z"), remap = false)
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;isEmpty()Z"))
     public boolean requestManaExactForTool(ItemStack stack) {
         if (noRepairItems.isEmpty()) {
             noRepairItems = Arrays.asList(BotaniaItems.terrasteelBoots, BotaniaItems.terrasteelChest,
