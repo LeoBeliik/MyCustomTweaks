6b3e0f6b7123d7327287a3a5a8d9cf77a10e110f
Leo <Leobrac23@gmail.com>
2023-03-25 07:00:25 -0300

    yeet item repair for terrasteel prevent other rods to swap with quark

--- src/main/java/com/leobeliik/mycustomtweaks/mixins/DontRestockMe.java
+++ src/main/java/com/leobeliik/mycustomtweaks/mixins/DontRestockMe.java
@@ -3,19 +3,38 @@ package com.leobeliik.mycustomtweaks.mixins;
 import net.minecraft.core.NonNullList;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import vazkii.botania.common.item.BotaniaItems;
 import vazkii.quark.content.management.module.AutomaticToolRestockModule;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Mixin(AutomaticToolRestockModule.class)
 public class DontRestockMe {
+
+
+    @Unique
+    private List<Item> dontRestock = new ArrayList<>();
 
     @Redirect(method = "Lvazkii/quark/content/management/module/AutomaticToolRestockModule;switchItems(Lnet/minecraft/world/entity/player/Player;II)V",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/core/NonNullList;size()I"), remap = false)
     private int findReplacement(NonNullList list, Player player, int slot1, int slot2) {
+        if (dontRestock.isEmpty()) {
+            dontRestock = Arrays.asList(BotaniaItems.exchangeRod, BotaniaItems.cobbleRod, BotaniaItems.dirtRod, BotaniaItems.skyDirtRod);
+        }
+
         Inventory inventory = player.getInventory();
-        return inventory.getItem(slot2).is(BotaniaItems.exchangeRod) ? 0 : inventory.items.size();
+        for (Item item : dontRestock) {
+            if (inventory.getItem(slot2).is(item)) {
+                return 0;
+            }
+        }
+        return inventory.items.size();
     }
 }
---
+++ src/main/java/com/leobeliik/mycustomtweaks/mixins/ManaRepairMixin.java
@@ -0,0 +1,31 @@
+package com.leobeliik.mycustomtweaks.mixins;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import vazkii.botania.common.impl.mana.ManaItemHandlerImpl;
+import vazkii.botania.common.item.BotaniaItems;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Mixin(ManaItemHandlerImpl.class)
+public class ManaRepairMixin {
+
+    @Unique
+    private List<Item> noRepairItems = new ArrayList<>();
+    @Redirect(method = "Lvazkii/botania/common/impl/mana/ManaItemHandlerImpl;requestManaExact(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/entity/player/Player;IZ)Z",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;isEmpty()Z"), remap = false)
+    public boolean requestManaExactForTool(ItemStack stack) {
+        if (noRepairItems.isEmpty()) {
+            noRepairItems = Arrays.asList(BotaniaItems.terrasteelBoots, BotaniaItems.terrasteelChest,
+                BotaniaItems.terrasteelHelm, BotaniaItems.terrasteelLegs, BotaniaItems.terraAxe, BotaniaItems.terraPick);
+        }
+
+        return noRepairItems.stream().anyMatch(stack::is);
+    }
+}
