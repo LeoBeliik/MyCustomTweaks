8538c362faf3710fa96ebc4374bc5533b2fd7532
Leo <Leobrac23@gmail.com>
2023-03-26 01:16:54 -0300

    Forgor the mage set

--- src/main/java/com/leobeliik/mycustomtweaks/mixins/ManaRepairMixin.java
+++ src/main/java/com/leobeliik/mycustomtweaks/mixins/ManaRepairMixin.java
@@ -2,18 +2,15 @@ package com.leobeliik.mycustomtweaks.mixins;
 
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.fml.loading.FMLLoader;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import vazkii.botania.common.impl.mana.ManaItemHandlerImpl;
 import vazkii.botania.common.item.BotaniaItems;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
 import static com.leobeliik.mycustomtweaks.MyCustomTweaks.isTetra;
 
 @Mixin(ManaItemHandlerImpl.class)
@@ -21,13 +18,15 @@ public class ManaRepairMixin {
 
     @Unique
     private List<Item> noRepairItems = new ArrayList<>();
+
     @Redirect(method = "Lvazkii/botania/common/impl/mana/ManaItemHandlerImpl;requestManaExact(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/entity/player/Player;IZ)Z",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;isEmpty()Z"))
     public boolean requestManaExactForTool(ItemStack stack) {
         if (isTetra) {
             if (noRepairItems.isEmpty()) {
-                noRepairItems = Arrays.asList(BotaniaItems.terrasteelBoots, BotaniaItems.terrasteelChest,
-                        BotaniaItems.terrasteelHelm, BotaniaItems.terrasteelLegs, BotaniaItems.terraAxe, BotaniaItems.terraPick);
+                noRepairItems = Arrays.asList(BotaniaItems.terrasteelBoots, BotaniaItems.terrasteelChest, BotaniaItems.terrasteelHelm,
+                        BotaniaItems.terrasteelLegs, BotaniaItems.terraAxe, BotaniaItems.terraPick, BotaniaItems.manaweaveBoots,
+                        BotaniaItems.manaweaveLegs, BotaniaItems.manaweaveChest, BotaniaItems.manaweaveHelm);
             }
             return noRepairItems.stream().anyMatch(stack::is);
         }
