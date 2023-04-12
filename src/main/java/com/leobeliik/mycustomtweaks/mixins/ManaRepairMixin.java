c542cde8c7f0849f2983e877753c1ad060c6fc7c
Leo <Leobrac23@gmail.com>
2023-04-12 08:45:58 -0300

    filter mana repair

--- src/main/java/com/leobeliik/mycustomtweaks/mixins/ManaRepairMixin.java
+++ src/main/java/com/leobeliik/mycustomtweaks/mixins/ManaRepairMixin.java
@@ -1,24 +1,19 @@
 package com.leobeliik.mycustomtweaks.mixins;
 
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import vazkii.botania.common.impl.mana.ManaItemHandlerImpl;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 @Mixin(ManaItemHandlerImpl.class)
 public class ManaRepairMixin {
 
-    @Unique
-    private List<Item> noRepairItems = new ArrayList<>();
-
     @Redirect(method = "Lvazkii/botania/common/impl/mana/ManaItemHandlerImpl;requestManaExact(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/entity/player/Player;IZ)Z",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;isEmpty()Z"))
     public boolean requestManaExactForTool(ItemStack stack) {
-        return true;
+        String[] ban = {"manasteel", "elementium", "terra", "glass", "sword", "bow", "manaweave"};
+        return Arrays.stream(ban).anyMatch(s -> stack.getDescriptionId().contains(s));
     }
 }
