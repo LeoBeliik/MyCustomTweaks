0a696e656bfaa3b64ebf7f5f6cae7ff9f4e462bc
Leo <LeoBrac23@gmail.com>
2023-06-30 11:19:02 -0300

    Maybe it's time to retire decorative blocks 🤔

--- src/main/java/com/leobeliik/mycustomtweaks/MyCustomTweaks.java
+++ src/main/java/com/leobeliik/mycustomtweaks/MyCustomTweaks.java
@@ -1,38 +1,28 @@
 package com.leobeliik.mycustomtweaks;
 
-import com.leobeliik.mycustomtweaks.items.PlayerSeedItem;
-import com.simibubi.create.content.kinetics.deployer.DeployerFakePlayer;
-import net.minecraft.tags.BlockTags;
+import lilypuree.decorative_blocks.blocks.SupportBlock;
+import net.minecraft.client.gui.components.EditBox;
+import net.minecraft.client.gui.components.events.GuiEventListener;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.monster.Creeper;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.enchantment.Enchantments;
-import net.minecraft.world.level.Explosion;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.entity.BaseContainerBlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.player.AttackEntityEvent;
-import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
-import net.minecraftforge.event.level.BlockEvent;
-import net.minecraftforge.event.level.ExplosionEvent;
 import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
-import vazkii.botania.common.block.block_entity.SimpleInventoryBlockEntity;
-import vazkii.botania.common.item.BotaniaItems;
-import vazkii.botania.common.item.rod.SkiesRodItem;
 
 import java.util.regex.Pattern;
 
@@ -43,36 +33,10 @@ public class MyCustomTweaks {
 
     private static final Pattern TORCH_PATTERN = Pattern.compile("(?:(?:(?:[A-Z-_.:]|^)torch)|(?:(?:[a-z-_.:]|^)Torch))(?:[A-Z-_.:]|$)");
 
-    //public static boolean isTetra = false;
 
     public MyCustomTweaks() {
         MinecraftForge.EVENT_BUS.register(this);
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-        Registry();
-        //isTetra = FMLLoader.getLoadingModList().getModFileById("tetra") != null;
-    }
-
-    private void Registry() {
-        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
-    }
-
-    @SubscribeEvent
-    public void onShatBreak(PlayerEvent.BreakSpeed event) {
-        ItemStack stack = event.getEntity().getItemInHand(InteractionHand.MAIN_HAND);
-        if (stack.is(BotaniaItems.terraPick) && event.getState().is(BlockTags.MINEABLE_WITH_SHOVEL)) {
-            int efficiency = stack.getItem().getEnchantmentLevel(stack, Enchantments.BLOCK_EFFICIENCY);
-            efficiency = efficiency == 0 ? 9 : (efficiency + 1) * 5; //numbers based on my ass
-            event.setNewSpeed(event.getOriginalSpeed() * efficiency);
-        }
-    }
-
-    @SubscribeEvent
-    public void onCreeperExplode(ExplosionEvent.Start event) {
-        if (event.getExplosion().getExploder() instanceof Creeper creeper) {
-            event.setCanceled(true);
-            float f = creeper.isPowered() ? 2.0F : 1.0F;
-            event.getLevel().explode(null, creeper.getX(), creeper.getY(), creeper.getZ(), 3 * f, false, Explosion.BlockInteraction.BREAK);
-        }
     }
 
     @SubscribeEvent
@@ -80,49 +44,31 @@ public class MyCustomTweaks {
         if (event.getItemStack().is(Blocks.WITHER_ROSE.asItem()) && event.getEntity().isCrouching()) {
             event.setUseItem(Event.Result.DENY);
         }
-        BlockEntity be = event.getLevel().getBlockEntity(event.getHitVec().getBlockPos());
-        if ((be instanceof BaseContainerBlockEntity || be instanceof SimpleInventoryBlockEntity)
-                && (event.getItemStack().is(BotaniaItems.corporeaSpark) || event.getItemStack().is(BotaniaItems.corporeaSparkMaster))) {
-            event.setUseBlock(Event.Result.DENY);
-        }
     }
 
-    @SubscribeEvent
-    public void onUseTornado(PlayerInteractEvent.RightClickItem event) {
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public void onBlockRclick(PlayerInteractEvent.RightClickBlock event) {
         Player player = event.getEntity();
-        ItemStack itemstack = event.getItemStack();
-
-        if (!player.isFallFlying() && player.getItemBySlot(EquipmentSlot.CHEST).canElytraFly(player)) {
-            Level level = player.level;
-            if (itemstack.getItem() instanceof SkiesRodItem tornado) {
-                player.startFallFlying();
-                player.jumpFromGround();
-
-                if (!level.isClientSide) {
-                    tornado.use(level, player, InteractionHand.MAIN_HAND);
+        Level level = event.getLevel();
+        BlockPos pos = event.getPos();
+        BlockState block = level.getBlockState(pos);
+        Item item = event.getItemStack().getItem();
+        InteractionHand hand = event.getHand();
+        if (event.getItemStack().getShareTag() != null) {
+            UseOnContext ctx = new UseOnContext(player, hand, event.getHitVec());
+            String tags = event.getItemStack().getShareTag().toString();
+            if ((tags.contains("axe") || tags.contains("adze")) && block.getBlock() instanceof SupportBlock) {
+                //tetra axe on decorative blocks
+                if (!ctx.getLevel().isClientSide) {
+                    SupportBlock.onSupportActivation(block, level, pos, player, ctx.getClickLocation());
+                } else {
+                    player.swing(hand);
                 }
-                event.setCanceled(true);
-                event.setCancellationResult(level.isClientSide ? InteractionResult.SUCCESS : InteractionResult.CONSUME);
             }
-
         }
     }
 
     @SubscribeEvent
-    public void noDeployPlacement(BlockEvent.EntityPlaceEvent event) {
-        if (event.getEntity() instanceof DeployerFakePlayer) {
-            event.setCanceled(true);
-        }
-    }
-
-    @SubscribeEvent
-    public void noDeployAttack(AttackEntityEvent event) {
-        if (event.getEntity() instanceof DeployerFakePlayer) {
-            event.setCanceled(true);
-        }
-    }
-
-    /*@SubscribeEvent
     public void tomPlzRclick(ScreenEvent.MouseButtonPressed event) {
         Screen screen = event.getScreen();
         if (screen.toString().contains("TerminalScreen")) {
@@ -133,8 +79,5 @@ public class MyCustomTweaks {
                 }
             }
         }
-    }*/
-
-    public static final RegistryObject<Item> PLAYER_SEED_ITEM = ITEMS.register("player_seed", () ->
-            new PlayerSeedItem(new Item.Properties().stacksTo(8)));
+    }
 }
