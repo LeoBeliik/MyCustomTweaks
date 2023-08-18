3b2381c4c2429aaddb913be1374d43100d77f483
Leo <LeoBrac23@gmail.com>
2023-08-18 14:40:17 -0300

    IE crates ain't shulker boxes

--- src/main/java/com/leobeliik/mycustomtweaks/MyCustomTweaks.java
+++ src/main/java/com/leobeliik/mycustomtweaks/MyCustomTweaks.java
@@ -1,8 +1,14 @@
 package com.leobeliik.mycustomtweaks;
 
+import blusunrize.immersiveengineering.common.blocks.wooden.WoodenCrateBlockEntity;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.Containers;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -22,5 +28,15 @@ public class MyCustomTweaks {
         MinecraftForge.EVENT_BUS.register(this);
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         //Registry();
+    }
+
+    @SubscribeEvent
+    public void OnBreakEvent(BlockEvent.BreakEvent event) {
+        Level level = event.getPlayer().level;
+        BlockPos pos = event.getPos();
+        //make IE crates drop the items
+        if (level.getBlockEntity(pos) instanceof WoodenCrateBlockEntity crate) {
+            Containers.dropContents(level, pos, crate);
+        }
     }
 }
