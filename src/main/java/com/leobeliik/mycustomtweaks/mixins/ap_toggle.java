9ca76515f086eed2a03da452c5132ebcb5e0232c
Leo <LeoBrac23@gmail.com>
2023-08-17 22:18:38 -0300

    parchment & additional placements need a hammer to work

---
+++ src/main/java/com/leobeliik/mycustomtweaks/mixins/ap_toggle.java
@@ -0,0 +1,22 @@
+package com.leobeliik.mycustomtweaks.mixins;
+
+import com.firemerald.additionalplacements.block.interfaces.IPlacementBlock;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+import java.util.Arrays;
+
+@Mixin(IPlacementBlock.class)
+public interface ap_toggle {
+
+    @Overwrite(remap = false)
+    default boolean disablePlacement(BlockPos pos, Level level, Direction direction) {
+        Player player = level.getNearestPlayer(pos.getX(), pos.getY(), pos.getZ(), 2.0D, null);
+        return player == null || !Arrays.toString(player.getItemInHand(InteractionHand.OFF_HAND).getTags().toArray()).contains("hammer");
+    }
+}
