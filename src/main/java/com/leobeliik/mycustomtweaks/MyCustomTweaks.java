8d27ae40898f5425a279dcf05f4343b1a6d68290
Leo <LeoBrac23@gmail.com>
2023-08-23 22:52:42 -0300

    better placards

--- src/main/java/com/leobeliik/mycustomtweaks/MyCustomTweaks.java
+++ src/main/java/com/leobeliik/mycustomtweaks/MyCustomTweaks.java
@@ -1,19 +1,26 @@
 package com.leobeliik.mycustomtweaks;
 
+import com.simibubi.create.AllSoundEvents;
+import com.simibubi.create.content.decoration.placard.PlacardBlock;
+import com.simibubi.create.content.decoration.placard.PlacardBlockEntity;
 import lilypuree.decorative_blocks.blocks.SupportBlock;
 import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.EventPriority;
@@ -21,10 +28,10 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.regex.Pattern;
+
+import static com.simibubi.create.content.decoration.placard.PlacardBlock.updateNeighbours;
 
 @Mod(MyCustomTweaks.MODID)
 public class MyCustomTweaks {
@@ -32,6 +39,8 @@ public class MyCustomTweaks {
     //private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
 
     private static final Pattern TORCH_PATTERN = Pattern.compile("(?:(?:(?:[A-Z-_.:]|^)torch)|(?:(?:[a-z-_.:]|^)Torch))(?:[A-Z-_.:]|$)");
+    private int poweredTicks;
+    private PlacardBlockEntity tickBE;
 
 
     public MyCustomTweaks() {
@@ -54,6 +63,27 @@ public class MyCustomTweaks {
         BlockState block = level.getBlockState(pos);
         Item item = event.getItemStack().getItem();
         InteractionHand hand = event.getHand();
+        if (block.getBlock() instanceof PlacardBlock pla) {
+            pla.onBlockEntityUse(level, pos, pte -> {
+                ItemStack inBlock = pte.getHeldItem();
+                if (inBlock.isEmpty()) {
+                    return null;
+                } else {
+                    if (player.isShiftKeyDown())
+                        return InteractionResult.PASS;
+                    if (level.isClientSide)
+                        return InteractionResult.SUCCESS;
+
+                    tickBE = pte;
+                    AllSoundEvents.CONFIRM.play(level, null, pos, 1, 1);
+                    level.setBlock(pos, block.setValue(BlockStateProperties.POWERED, true), 3);
+                    updateNeighbours(block, level, pos);
+                    this.poweredTicks = 19;
+                    pte.notifyUpdate();
+                    return InteractionResult.SUCCESS;
+                }
+            });
+        }
         if (event.getItemStack().getShareTag() != null) {
             UseOnContext ctx = new UseOnContext(player, hand, event.getHitVec());
             String tags = event.getItemStack().getShareTag().toString();
@@ -67,6 +97,27 @@ public class MyCustomTweaks {
             }
         }
     }
+
+    @SubscribeEvent
+    public void onTick(TickEvent event) {
+        if (tickBE != null) {
+            Level level = tickBE.getLevel();
+            BlockPos pos = tickBE.getBlockPos();
+            if (level.isClientSide)
+                return;
+            if (poweredTicks == 0)
+                return;
+
+            poweredTicks--;
+            if (poweredTicks > 0)
+                return;
+
+            BlockState blockState = tickBE.getBlockState();
+            level.setBlock(pos, blockState.setValue(PlacardBlock.POWERED, false), 3);
+            PlacardBlock.updateNeighbours(blockState, level, pos);
+        }
+    }
+
 
     @SubscribeEvent
     public void tomPlzRclick(ScreenEvent.MouseButtonPressed event) {
