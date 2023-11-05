8ba8ed4b7b7351d60fedc6750a91c8e2be884948
Leo <Leobrac23@gmail.com>
2023-11-04 21:47:54 -0300

    fix quark double door opening

--- src/main/java/com/leobeliik/mycustomtweaks/MyCustomTweaks.java
+++ src/main/java/com/leobeliik/mycustomtweaks/MyCustomTweaks.java
@@ -2,12 +2,23 @@ package com.leobeliik.mycustomtweaks;
 
 import blusunrize.immersiveengineering.common.blocks.wooden.WoodenCrateBlockEntity;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.Containers;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.DoorBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.DoorHingeSide;
+import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.client.event.ContainerScreenEvent;
 import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
@@ -15,12 +26,18 @@ import net.minecraftforge.event.entity.item.ItemEvent;
 import net.minecraftforge.event.entity.player.PlayerContainerEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import vazkii.quark.base.module.ModuleLoader;
+import vazkii.quark.base.network.QuarkNetwork;
+import vazkii.quark.base.network.message.DoubleDoorMessage;
+import vazkii.quark.content.tweaks.module.DoubleDoorOpeningModule;
 
 import java.util.regex.Pattern;
 
@@ -46,5 +63,25 @@ public class MyCustomTweaks {
         if (level.getBlockEntity(pos) instanceof WoodenCrateBlockEntity crate) {
             Containers.dropContents(level, pos, crate);
         }
+    }
+
+    //quark double door opening
+    @SubscribeEvent(priority = EventPriority.LOW)
+    public void onPlayerInteract(PlayerInteractEvent.RightClickBlock event) {
+        if(!event.getWorld().isClientSide || event.getPlayer().isDiscrete() || event.isCanceled() || event.getResult() == Event.Result.DENY || event.getUseBlock() == Event.Result.DENY) {
+            return;
+        }
+
+        Level world = event.getWorld();
+        BlockPos pos = event.getPos();
+
+        if(isDoor(world.getBlockState(pos))) {
+            DoubleDoorOpeningModule.openDoor(world, event.getPlayer(), pos);
+            QuarkNetwork.sendToServer(new DoubleDoorMessage(pos));
+        }
+    }
+
+    private static boolean isDoor(BlockState state) {
+        return state.getBlock() instanceof DoorBlock;
     }
 }
