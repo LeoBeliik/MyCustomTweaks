a387f0700e7ea999720ba662f77e9f3035b85244
Leo <Leobrac23@gmail.com>
2023-11-06 20:58:14 -0300

    1.20 real

--- src/main/java/com/leobeliik/mycustomtweaks/MyCustomTweaks.java
+++ src/main/java/com/leobeliik/mycustomtweaks/MyCustomTweaks.java
@@ -1,87 +1,45 @@
 package com.leobeliik.mycustomtweaks;
 
-import blusunrize.immersiveengineering.common.blocks.wooden.WoodenCrateBlockEntity;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.ItemTags;
-import net.minecraft.tags.TagKey;
-import net.minecraft.world.Containers;
-import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.monster.Creeper;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.DoorBlock;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.DoorHingeSide;
-import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
-import net.minecraft.world.level.material.Material;
-import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.phys.HitResult;
-import net.minecraft.world.phys.Vec3;
-import net.minecraftforge.client.event.ContainerScreenEvent;
-import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.item.ItemEvent;
-import net.minecraftforge.event.entity.player.PlayerContainerEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
-import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.event.level.ExplosionEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import vazkii.quark.base.module.ModuleLoader;
-import vazkii.quark.base.network.QuarkNetwork;
-import vazkii.quark.base.network.message.DoubleDoorMessage;
-import vazkii.quark.content.tweaks.module.DoubleDoorOpeningModule;
-
-import java.util.regex.Pattern;
 
 @Mod(MyCustomTweaks.MODID)
 public class MyCustomTweaks {
     static final String MODID = "mycustomtweaks";
-    private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
-
-    private static final Pattern TORCH_PATTERN = Pattern.compile("(?:(?:(?:[A-Z-_.:]|^)torch)|(?:(?:[a-z-_.:]|^)Torch))(?:[A-Z-_.:]|$)");
-    public static TagKey<Item> bundeableTag;
 
     public MyCustomTweaks() {
         MinecraftForge.EVENT_BUS.register(this);
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-        bundeableTag = ItemTags.create(new ResourceLocation(MODID, "bundeable"));
     }
 
     @SubscribeEvent
-    public void OnBreakEvent(BlockEvent.BreakEvent event) {
-        Level level = event.getPlayer().level;
-        BlockPos pos = event.getPos();
-        //make IE crates drop the items
-        if (level.getBlockEntity(pos) instanceof WoodenCrateBlockEntity crate) {
-            Containers.dropContents(level, pos, crate);
+    public void onCreeperExplode(ExplosionEvent.Start event) {
+        if (event.getExplosion().getExploder() instanceof Creeper creeper) {
+            event.setCanceled(true);
+            float f = creeper.isPowered() ? 2.0F : 1.0F;
+            event.getLevel().explode(null, creeper.getX(), creeper.getY(), creeper.getZ(), 3f * f, false, Level.ExplosionInteraction.TNT);
         }
     }
 
-    //quark double door opening
     @SubscribeEvent(priority = EventPriority.LOW)
     public void onPlayerInteract(PlayerInteractEvent.RightClickBlock event) {
-        if(!event.getWorld().isClientSide || event.getPlayer().isDiscrete() || event.isCanceled() || event.getResult() == Event.Result.DENY || event.getUseBlock() == Event.Result.DENY) {
+        Player player = event.getEntity();
+        if (!event.getLevel().isClientSide || player.isDiscrete() || event.isCanceled() || event.getResult() == Event.Result.DENY || event.getUseBlock() == Event.Result.DENY)
             return;
-        }
 
-        Level world = event.getWorld();
+        Level world = event.getLevel();
         BlockPos pos = event.getPos();
-
-        if(isDoor(world.getBlockState(pos))) {
-            DoubleDoorOpeningModule.openDoor(world, event.getPlayer(), pos);
-            QuarkNetwork.sendToServer(new DoubleDoorMessage(pos));
-        }
     }
 
-    private static boolean isDoor(BlockState state) {
-        return state.getBlock() instanceof DoorBlock;
-    }
 }
