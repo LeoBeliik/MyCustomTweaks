b1a24f7333bca169c57931b2d5759a553f61efe5
Leo <Leobrac23@gmail.com>
2024-02-22 19:56:10 -0300

    new pack

--- src/main/java/com/leobeliik/mycustomtweaks/MyCustomTweaks.java
+++ src/main/java/com/leobeliik/mycustomtweaks/MyCustomTweaks.java
@@ -1,135 +1,17 @@
 package com.leobeliik.mycustomtweaks;
 
-import blusunrize.immersiveengineering.common.blocks.wooden.WoodenCrateBlockEntity;
-import com.leobeliik.mycustomtweaks.Network.JustStackIt;
-import com.leobeliik.mycustomtweaks.Network.Network;
-import com.mojang.blaze3d.platform.InputConstants;
-import net.dries007.tfc.client.TFCKeyBindings;
-import net.dries007.tfc.common.TFCEffects;
-import net.dries007.tfc.common.blockentities.ThatchBedBlockEntity;
-import net.dries007.tfc.common.blocks.RiverWaterBlock;
-import net.dries007.tfc.common.blocks.TFCBlockStateProperties;
-import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
-import net.minecraft.core.BlockPos;
-import net.minecraft.network.chat.Component;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.Containers;
-import net.minecraft.world.effect.MobEffect;
-import net.minecraft.world.effect.MobEffectCategory;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.entity.monster.Creeper;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.inventory.Slot;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.material.Fluids;
-import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.player.PlayerSleepInBedEvent;
-import net.minecraftforge.event.entity.player.PlayerWakeUpEvent;
-import net.minecraftforge.event.level.BlockEvent;
-import net.minecraftforge.event.level.ExplosionEvent;
-import net.minecraftforge.eventbus.api.Event;
-import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.RegistryObject;
-
-import static net.dries007.tfc.common.TFCEffects.register;
 
 @Mod(MyCustomTweaks.MODID)
 public class MyCustomTweaks {
     public static final String MODID = "mycustomtweaks";
-    public static final RegistryObject<MobEffect> INSOMNIA = register("insomnia", () -> new TFCEffects.TFCMobEffect(MobEffectCategory.BENEFICIAL, 0));
-    private long time;
 
     public MyCustomTweaks() {
         MinecraftForge.EVENT_BUS.register(this);
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-        Network.register();
-    }
-
-    @SubscribeEvent
-    public void OnBreakEvent(BlockEvent.BreakEvent event) {
-        Level level = event.getPlayer().level();
-        BlockPos pos = event.getPos();
-        //make IE crates drop the items
-        if (level.getBlockEntity(pos) instanceof WoodenCrateBlockEntity crate) {
-            Containers.dropContents(level, pos, crate);
-        }
-    }
-
-    @SubscribeEvent
-    public void onCreeperExplode(ExplosionEvent.Start event) {
-        if (event.getExplosion().getExploder() instanceof Creeper creeper) {
-            event.setCanceled(true);
-            float f = creeper.isPowered() ? 2.0F : 1.0F;
-            event.getLevel().explode(null, creeper.getX(), creeper.getY(), creeper.getZ(), 3f * f, false, Level.ExplosionInteraction.TNT);
-        }
-    }
-
-    @SubscribeEvent
-    public void onPlayerWakingUp(PlayerWakeUpEvent event) {
-        Player player = event.getEntity();
-        Level level = event.getEntity().level();
-
-        if (time != 0) {
-            if (level instanceof ServerLevel) {
-                long currentTime = level.getDayTime();
-                if (time % 24000 < 18000) {
-                    ((ServerLevel) level).setDayTime(currentTime - ((currentTime - time) / 2));
-                    player.displayClientMessage(Component.nullToEmpty("You struggle to keep sleeping"), true);
-                    player.addEffect(new MobEffectInstance(INSOMNIA.get(), 6000, -1, false, false, false));
-                }
-            }
-            time = 0;
-        }
-    }
-
-    @SubscribeEvent
-    public void onPlayerSleeping(PlayerSleepInBedEvent event) {
-        Player player = event.getEntity();
-        Level level = player.level();
-        if (level.getBlockEntity(event.getPos()) instanceof ThatchBedBlockEntity) {
-            if (player.hasEffect(INSOMNIA.get()) && event.getResult().equals(Event.Result.DEFAULT)) {
-                player.displayClientMessage(Component.nullToEmpty("You can't go to sleep now"), true);
-                event.setResult(Player.BedSleepingProblem.OTHER_PROBLEM);
-            } else {
-                time = level.getDayTime();
-            }
-        }
-    }
-
-    @SubscribeEvent
-    public void onBlockPlaced(BlockEvent.EntityPlaceEvent event) {
-        if (event.getBlockSnapshot().getReplacedBlock().getBlock() instanceof RiverWaterBlock) {
-            Waterlog(event.getPlacedBlock(), (Level) event.getLevel(), event.getPos());
-        }
-    }
-
-    @SubscribeEvent
-    public void onBlockPlaced(BlockEvent.EntityMultiPlaceEvent event) {
-        if (event.getBlockSnapshot().getReplacedBlock().getBlock() instanceof RiverWaterBlock) {
-            Waterlog(event.getPlacedBlock(), (Level) event.getLevel(), event.getPos());
-        }
-    }
-
-    @SubscribeEvent(priority = EventPriority.LOW)
-    public void onScreenKey(ScreenEvent.KeyPressed event) {
-        if (TFCKeyBindings.STACK_FOOD.isActiveAndMatches(InputConstants.getKey(event.getKeyCode(), event.getScanCode())) && event.getScreen() instanceof AbstractContainerScreen inv) {
-            Slot slot = inv.getSlotUnderMouse();
-            if (slot != null) {
-                Network.sendToServer(new JustStackIt(slot.index));
-            }
-        }
-    }
-
-    private void Waterlog(BlockState block, Level level, BlockPos pos) {
-        BlockState state = block.trySetValue(BlockStateProperties.WATERLOGGED, true).trySetValue(TFCBlockStateProperties.WATER, TFCBlockStateProperties.WATER.keyFor(Fluids.WATER));
-        level.setBlock(pos, state, 0);
     }
 
 }
--- src/main/java/com/leobeliik/mycustomtweaks/mixins/invButtonMixin.java
+++ src/main/java/com/leobeliik/mycustomtweaks/mixins/invButtonMixin.java
@@ -1,12 +1,11 @@
 package com.leobeliik.mycustomtweaks.mixins;
 
-import net.dries007.tfc.client.screen.LargeVesselScreen;
-import net.dries007.tfc.common.container.LargeVesselContainer;
+import me.desht.pneumaticcraft.client.gui.SmartChestScreen;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
-import net.minecraft.client.gui.screens.recipebook.RecipeUpdateListener;
 import net.minecraft.network.chat.Component;
+import org.checkerframework.common.aliasing.qual.Unique;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -20,9 +19,17 @@ import java.util.function.Supplier;
 @Mixin(MiniInventoryButton.class)
 public class invButtonMixin extends Button {
 
-    @Shadow @Final private AbstractContainerScreen<?> parent;
+    @Shadow
+    @Final
+    private AbstractContainerScreen<?> parent;
 
-    @Shadow @Final private Supplier<List<Component>> tooltip;
+    @Shadow
+    @Final
+    private Supplier<List<Component>> tooltip;
+
+    @Shadow @Final private int startX;
+
+    @Unique private int i = 0;
 
     protected invButtonMixin(int pX, int pY, int pWidth, int pHeight, Component pMessage, OnPress pOnPress, CreateNarration pCreateNarration) {
         super(pX, pY, pWidth, pHeight, pMessage, pOnPress, pCreateNarration);
@@ -30,22 +37,15 @@ public class invButtonMixin extends Button {
 
     @Overwrite
     public void render(@NotNull GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTicks) {
-        boolean sortButton = this.tooltip.get().toString().contains("button.sort_container");
-        boolean filterButton = this.tooltip.get().toString().contains("button.filter");
-        if (sortButton || filterButton) {
-            this.setX(this.parent.getGuiLeft() + this.parent.getXSize() - (filterButton ? 30 : 18));
-            this.setY(this.parent.getGuiTop() + 5);
-        }
-        super.render(guiGraphics, mouseX, mouseY, partialTicks);
-    }
-
-    @Override
-    public void onPress() {
-        if (parent instanceof LargeVesselScreen vessel && this.tooltip.get().toString().contains("extract")) {
-            if (vessel.getMenu().isSealed()) {
-                return;
+        if (parent instanceof SmartChestScreen) {
+            boolean sortButton = this.tooltip.get().toString().contains("button.sort_container");
+            boolean filterButton = this.tooltip.get().toString().contains("button.filter");
+            if (!sortButton && !filterButton && i == 0) {
+                i++;
+                this.setX(this.getX() + 55);
+                this.setY(this.getY() + 10);
             }
         }
-        super.onPress();
+        super.render(guiGraphics, mouseX, mouseY, partialTicks);
     }
 }
