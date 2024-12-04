37a08b1b08a5779983e5f1beb78f3efb35dfe42a
Leo <Leobrac23@gmail.com>
2024-12-04 14:49:22 -0300

    tired of the dumb book not closing with e

--- src/main/java/com/leobeliik/mycustomtweaks/MyCustomTweaks.java
+++ src/main/java/com/leobeliik/mycustomtweaks/MyCustomTweaks.java
@@ -1,14 +1,23 @@
 package com.leobeliik.mycustomtweaks;
 
+import com.klikli_dev.modonomicon.client.gui.book.*;
 import com.tiviacz.travelersbackpack.handlers.EntityItemHandler;
 import dan200.computercraft.api.ComputerCraftAPI;
 import de.dafuqs.spectrum.blocks.decoration.DecayingLightBlock;
 import de.dafuqs.spectrum.blocks.decoration.WandLightBlock;
 import de.dafuqs.spectrum.registries.SpectrumItems;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
+import net.fabricmc.fabric.api.client.screen.v1.ScreenKeyboardEvents;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.EditBox;
+import net.minecraft.client.gui.Element;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.option.KeyBinding;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
@@ -26,23 +35,26 @@ public class MyCustomTweaks implements ModInitializer {
 
     public void onInitialize() {
         ComputerCraftCompat.register();
-        PlayerBlockBreakEvents.AFTER.register(new lightBreak());
+        PlayerBlockBreakEvents.AFTER.register((world, playerEntity, blockPos, blockState, blockEntity) -> {
+            if (blockState.getBlock() instanceof WandLightBlock lightBlock && !(lightBlock instanceof DecayingLightBlock)) {
+                world.spawnEntity(new ItemEntity(world, blockPos.getX(), blockPos.getY(), blockPos.getZ(),
+                        SpectrumItems.SHIMMERSTONE_GEM.getDefaultStack(), 0, 0.1, 0));
+            }
+        });
+
+        ScreenEvents.AFTER_INIT.register((minecraftClient, screen, i, i1) -> {
+            KeyBinding inventoryKey = minecraftClient.options.inventoryKey;
+            ScreenKeyboardEvents.afterKeyPress(screen).register((s, j, j1, j2) -> {
+                if ((s instanceof BookOverviewScreen || s instanceof BookContentScreen) && inventoryKey.matchesKey(j, j1)) {
+                    s.close();
+                }
+            });
+        });
     }
 
     public class ComputerCraftCompat {
         static void register() {
             ComputerCraftAPI.registerGenericSource(new ColorPickerPeripheral());
-        }
-    }
-
-    public class lightBreak implements PlayerBlockBreakEvents.After {
-
-        @Override
-        public void afterBlockBreak(World world, PlayerEntity playerEntity, BlockPos blockPos, BlockState blockState, @Nullable BlockEntity blockEntity) {
-            if (blockState.getBlock() instanceof WandLightBlock lightBlock && !(lightBlock instanceof DecayingLightBlock)) {
-                world.spawnEntity(new ItemEntity(world, blockPos.getX(), blockPos.getY(), blockPos.getZ(),
-                        SpectrumItems.SHIMMERSTONE_GEM.getDefaultStack(), 0, 0.1, 0));
-            }
         }
     }
 }
