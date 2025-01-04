02d49c8b25575f2f5a316beac37c2b3a68c14a4a
Leo <Leobrac23@gmail.com>
2025-01-04 13:42:17 -0300

    quarkit

--- src/main/java/com/leobeliik/mycustomtweaks/mixins/itemInBundleMixin.java
+++ src/main/java/com/leobeliik/mycustomtweaks/mixins/itemInBundleMixin.java
@@ -10,7 +10,6 @@ import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.BundleItem;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -19,7 +18,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class itemInBundleMixin {
 
     @Inject(method = "Lnet/minecraft/world/item/BundleItem;overrideOtherStackedOnMe(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/inventory/Slot;Lnet/minecraft/world/inventory/ClickAction;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/entity/SlotAccess;)Z",
-            at = @At("HEAD"), remap = false, cancellable = true)
+            at = @At("HEAD"), cancellable = true)
     public boolean overrideOtherStackedOnMe(ItemStack pStack, ItemStack pOther, Slot pSlot, ClickAction pAction, Player pPlayer, SlotAccess pAccess, CallbackInfoReturnable<Boolean> cir) {
         if (!ItemSizeManager.get(pOther).getSize(pOther).isSmallerThan(Size.NORMAL) && !ItemSizeManager.get(pOther).getWeight(pOther).isSmallerThan(Weight.MEDIUM)) {
             cir.setReturnValue(false);
@@ -29,7 +28,7 @@ public class itemInBundleMixin {
     }
 
     @Inject(method = "Lnet/minecraft/world/item/BundleItem;overrideStackedOnOther(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/inventory/Slot;Lnet/minecraft/world/inventory/ClickAction;Lnet/minecraft/world/entity/player/Player;)Z",
-            at = @At("HEAD"), remap = false, cancellable = true)
+            at = @At("HEAD"), cancellable = true)
     public boolean overrideStackedOnOther(ItemStack pStack, Slot pSlot, ClickAction pAction, Player pPlayer, CallbackInfoReturnable<Boolean> cir) {
         ItemStack itemstack = pSlot.getItem();
         if (!ItemSizeManager.get(itemstack).getSize(itemstack).isSmallerThan(Size.NORMAL) && !ItemSizeManager.get(itemstack).getWeight(itemstack).isSmallerThan(Weight.MEDIUM)) {
---
+++ src/main/java/com/leobeliik/mycustomtweaks/mixins/itemInQBackpackMixin.java
@@ -0,0 +1,21 @@
+package com.leobeliik.mycustomtweaks.mixins;
+
+import net.dries007.tfc.common.capabilities.size.ItemSizeManager;
+import net.dries007.tfc.common.capabilities.size.Size;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.violetmoon.quark.addons.oddities.inventory.slot.BackpackSlot;
+
+@Mixin(BackpackSlot.class)
+public class itemInQBackpackMixin {
+
+    @Inject(at = @At("RETURN"), method = "Lorg/violetmoon/quark/addons/oddities/inventory/slot/BackpackSlot;mayPlace(Lnet/minecraft/world/item/ItemStack;)Z",
+            remap = false)
+    public boolean mayPlace(@NotNull ItemStack stack, CallbackInfoReturnable cir) {
+        return ItemSizeManager.get(stack).getSize(stack).isEqualOrSmallerThan(Size.NORMAL);
+    }
+}
