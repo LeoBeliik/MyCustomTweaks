1c4159d7969537929e73da123aa924459c24c98e
Leo <Leobrac23@gmail.com>
2025-01-03 22:08:01 -0300

    tfc bundles

---
+++ src/main/java/com/leobeliik/mycustomtweaks/mixins/itemInBundleMixin.java
@@ -0,0 +1,41 @@
+package com.leobeliik.mycustomtweaks.mixins;
+
+import net.dries007.tfc.common.capabilities.size.ItemSizeManager;
+import net.dries007.tfc.common.capabilities.size.Size;
+import net.dries007.tfc.common.capabilities.size.Weight;
+import net.minecraft.world.entity.SlotAccess;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.ClickAction;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.BundleItem;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(BundleItem.class)
+public class itemInBundleMixin {
+
+    @Inject(method = "Lnet/minecraft/world/item/BundleItem;overrideOtherStackedOnMe(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/inventory/Slot;Lnet/minecraft/world/inventory/ClickAction;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/entity/SlotAccess;)Z",
+            at = @At("HEAD"), remap = false, cancellable = true)
+    public boolean overrideOtherStackedOnMe(ItemStack pStack, ItemStack pOther, Slot pSlot, ClickAction pAction, Player pPlayer, SlotAccess pAccess, CallbackInfoReturnable<Boolean> cir) {
+        if (!ItemSizeManager.get(pOther).getSize(pOther).isSmallerThan(Size.NORMAL) && !ItemSizeManager.get(pOther).getWeight(pOther).isSmallerThan(Weight.MEDIUM)) {
+            cir.setReturnValue(false);
+            cir.cancel();
+        }
+        return cir.getReturnValueZ();
+    }
+
+    @Inject(method = "Lnet/minecraft/world/item/BundleItem;overrideStackedOnOther(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/inventory/Slot;Lnet/minecraft/world/inventory/ClickAction;Lnet/minecraft/world/entity/player/Player;)Z",
+            at = @At("HEAD"), remap = false, cancellable = true)
+    public boolean overrideStackedOnOther(ItemStack pStack, Slot pSlot, ClickAction pAction, Player pPlayer, CallbackInfoReturnable<Boolean> cir) {
+        ItemStack itemstack = pSlot.getItem();
+        if (!ItemSizeManager.get(itemstack).getSize(itemstack).isSmallerThan(Size.NORMAL) && !ItemSizeManager.get(itemstack).getWeight(itemstack).isSmallerThan(Weight.MEDIUM)) {
+            cir.setReturnValue(false);
+            cir.cancel();
+        }
+        return cir.getReturnValueZ();
+    }
+}
